ee5f8f79cbde9b1d1b6d392e8cd496992b97d0a1
DHANUSH BABU RAMADOSS <dhanushbaburamadoss@mac.lan>
2025-03-20 23:57:55 -0400

    Initial commit: Instagram API backend

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    profilePic: { type: String, default: "" },
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('User', UserSchema);
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const authRoutes = require('./routes/auth');
+// const postRoutes = require('./routes/posts');
+// const userRoutes = require('./routes/users');
+
+// console.log("authRoutes",authRoutes);
+dotenv.config(); 
+connectDB(); 
+
+const app = express();
+app.use(express.json());
+app.use(cors()); 
+
+app.use('/api/auth', authRoutes);
+// app.use('/api/posts', postRoutes);
+// app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
